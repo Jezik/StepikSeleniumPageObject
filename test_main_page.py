31acbfc80ab5045f16d89c6641b0a7b933ee44d6
Dmitry Baev <dmitry.baevtech@gmail.com>
2020-01-08 12:05:22 +0300

    Modify main_page and test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
+from .pages.main_page import MainPage
+
 def go_to_login_page(driver):
     login_link = driver.find_element_by_css_selector("#login_link")
     login_link.click()
 
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    go_to_login_page(driver)
+    page = MainPage(driver, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
