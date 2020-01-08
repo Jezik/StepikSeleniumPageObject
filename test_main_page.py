a5968b35f4a6ca6e9a8ff4e8db64d59c88c845ed
Dmitry Baev <dmitry.baevtech@gmail.com>
2020-01-08 11:51:13 +0300

    Add method go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
+def go_to_login_page(driver):
+    login_link = driver.find_element_by_css_selector("#login_link")
+    login_link.click()
+
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     driver.get(link)
-    login_link = driver.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(driver)
